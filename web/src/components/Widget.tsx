7c8796d441dabd04dd6a89d2a6d6774b8121dca9
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2022-05-04 09:44:03 -0300

    Web project

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,19 @@
+import { ChatTeardropDots } from 'phosphor-react';
+import { Popover } from '@headlessui/react';
+import { WidgetForm } from './WidgetForm';
+
+export function Widget() {
+  return (
+    <Popover className="absolute right-4 bottom-4 md:right-8 md:bottom-8 flex flex-col items-end">
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
+
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">Feedback</span>
+      </Popover.Button>
+    </Popover>
+  )
+}
